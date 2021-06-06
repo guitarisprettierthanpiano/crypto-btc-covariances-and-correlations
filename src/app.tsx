72f7a923a1119aae02825fdfb25a977e61967b20
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-06-06 00:23:08 -0400

    correlation coeffecient done

--- src/app.tsx
+++ src/app.tsx
@@ -3,19 +3,21 @@ import { useState, useEffect, useRef } from 'react';
 
 const App = () => {
 
-    let data = [];
-    let btcVolatility = 0;
-    let btcMean = 0;
-    let btcPriceArray = [];
-    let btcReturnsArray = [];
-    let altPriceArray = [];
-    let altReturnsArray = [];
-    let mean = 0;
-    let covariance = 0;
-    let correlation = 0;
-    let variance = 0;
-    let volatility = 0;
-    let meansqrd = 0;
+    //some of these variables I need to keep outside the following function because I recall them consistantly after every fetch. So I declared them all here so I can find them easily.
+    let data:any[] = [];
+    let btcVolatility:number = 0;
+    let btcMean:number = 0;
+    let btcPriceArray:any[] = [];
+    let btcReturnsArray:any[] = [];
+    let altPriceArray:any[] = [];
+    let altReturnsArray:any[] = [];
+    let mean:number = 0;
+    let meanOfTheSquare:number = 0;
+    let variance:number = 0;
+    let volatility:number = 0;
+    let covariance:number = 0;
+    let correlation:number = 0;
+
     async function FetchNOW(){
         const goFetch = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`)
         .then(res => res.json())
@@ -38,7 +40,7 @@ const App = () => {
                 myTR.appendChild(myCoin2);
 
                 let myCoin3 = document.createElement('td');
-                myCoin3.innerText = result[i].market_cap;
+                myCoin3.innerText = result[i].market_cap.toLocaleString();
                 myTR.appendChild(myCoin3);
 
                 document.querySelector('.coin-table').appendChild(myTR);                
@@ -47,47 +49,38 @@ const App = () => {
                 .then(res => res.json())
                 .then(resulto => {
     
-                    if (result[i].id==='internet-computer'){
+                    if (result[i].id===('internet-computer'||'usd-coin')){
                         return false;
                     }
 
                     altPriceArray = [];
-                    altReturnsArray = [];
                     for(let a = 0; a < 31; a++){
                         altPriceArray.push(resulto.prices[a][1])
                     }
 
-                    //SECOND TRY
-                    mean = 0;
+                    altReturnsArray = [];
                     for (let b = 0; b < 30; b++){
-                        let returns = (altPriceArray[b+1]-altPriceArray[b])
+                        let returns = 100*((altPriceArray[b+1]-altPriceArray[b])
                         /
-                        (altPriceArray[b]);
+                        (altPriceArray[b]));
 
                         altReturnsArray.push(returns);
-                        mean += returns;
                     };
 
-                    mean = ((altPriceArray[30]-altPriceArray[0])/altPriceArray[0])
-                    mean *= (10/3);
+                    //36146 - 56507 / 56507 = -.36
+                    mean = 0;
+                    for (let d = 0; d < 30; d++){
+                        mean += (altReturnsArray[d]/30);
+                    }
 
-                    meansqrd = 0;
+                    meanOfTheSquare = 0;
                     for (let c = 0; c < 30; c++) {
-                        meansqrd += Math.pow((((altPriceArray[c + 1] - altPriceArray[c])/(altPriceArray[c])) - mean), 2);
+                        meanOfTheSquare += Math.pow((altReturnsArray[c]), 2)/30;
                     };
-
-                    meansqrd = (10000*((altPriceArray[30] - altPriceArray[0]))
-                    *
-                    (altPriceArray[30] - altPriceArray[0])) 
-                    / 
-                    (altPriceArray[0] * altPriceArray[0]);
-
-                    meansqrd /= 30;
 
                     variance = 0;
-                    variance = meansqrd - mean*mean;
-
                     volatility = 0;
+                    variance = meanOfTheSquare - Math.pow(mean, 2);
                     volatility = Math.sqrt(variance);
         
                     let newTD = document.createElement('td')
@@ -99,40 +92,36 @@ const App = () => {
                     document.querySelector(`.${data[i]}`).appendChild(newerTD);
         
                     let newestTD = document.createElement('td')
-                    newestTD.innerText = volatility.toFixed(2).toString()
+                    newestTD.innerText = volatility.toFixed(2).toString() + '%'
                     document.querySelector(`.${data[i]}`).appendChild(newestTD);
 
                     if (result[i].id === 'bitcoin') {
                         btcMean = mean;
-                        btcVolatility = Math.sqrt(volatility);
+                        btcVolatility = volatility;
                         btcReturnsArray = altReturnsArray;
 
-                        for (let j = 0; j < 31; j++){
+                        for (let j = 0; j < 30; j++){
                             btcPriceArray.push(resulto.prices[j][1])
                         }
-                    }
-                    else{
                     }
 
                     covariance = 0;
                     correlation = 0;                    
                     for (let k = 0; k < 30; k++){
-                        covariance += 
-                        ((altReturnsArray[k]
-                        - mean)
-                        *
-                        (btcReturnsArray[k]
-                        - btcMean))/29
+                        covariance += (
+                        ((altReturnsArray[k] - mean)
+                        *(btcReturnsArray[k]- btcMean)) 
+                        / 30);
                     }
-                    
-                    correlation = ((covariance)/(btcVolatility*volatility));
+
+                    correlation = covariance/(btcVolatility*volatility);
 
                     let anotherTD = document.createElement('td');
                     anotherTD.innerText = covariance.toFixed(2).toString();
                     document.querySelector(`.${data[i]}`).appendChild(anotherTD);
 
                     let finalTD = document.createElement('td');
-                    finalTD.innerText = correlation.toFixed(2).toString();
+                    finalTD.innerText = correlation.toFixed(4).toString();
                     document.querySelector(`.${data[i]}`).appendChild(finalTD);
                    
                 }
@@ -152,15 +141,15 @@ const App = () => {
     <div>
         <table className='coin-table'>
             <tr className='tr1'>
-                <th>#</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Market Cap</th>
-                <th>Average Return</th>
-                <th>Variance</th>
-                <th>Volatility</th>
-                <th>Covariance</th>
-                <th>Correlation</th>
+                <th className='rank'>#</th>
+                <th className='name'>Name</th>
+                <th className='price'>Price</th>
+                <th className='market-cap'>Market Cap</th>
+                <th className='return'>Daily Return</th>
+                <th className='var'>Variance</th>
+                <th className='sig'>Volatility</th>
+                <th className='covar'>Covariance</th>
+                <th className='cor'>Correlation</th>
             </tr>
         </table>
     </div>
